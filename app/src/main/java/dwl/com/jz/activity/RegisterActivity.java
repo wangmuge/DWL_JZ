351e2425e63b7da4cb02e6da25b08dd472d09175
wangmuge <342462109@qq.com>
2019-02-14 13:58:51 +0800

    三期功能-注册修改

--- app/src/main/java/dwl/com/jz/activity/RegisterActivity.java
+++ app/src/main/java/dwl/com/jz/activity/RegisterActivity.java
@@ -15,6 +15,8 @@ public class RegisterActivity extends BaseActivity {
     EditText etAccount;
     @BindView(R.id.et_password)
     EditText etPassword;
+    @BindView(R.id.et_password2)
+    EditText etPassword2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +34,23 @@ public class RegisterActivity extends BaseActivity {
             case R.id.tv_reg:
                 String username = etAccount.getText().toString().trim();
                 String psw = etPassword.getText().toString().trim();
-                showToast("注册成功！");
-                setUser(username,psw);
-                finish();
+                String psw2 = etPassword2.getText().toString().trim();
+                if(username.length()>0 && psw.length()>0 &&psw2.length()>0) {
+                    String regex = "[a-zA-Z]\\w{5,9}";
+                    if (!username.matches(regex)) {
+                        showToast("用户名要以字母开头，只能包括字母,下划线,数字，长度必须在6到10之间");
+                        return;
+                    }
+                    if (!psw.equals(psw2)) {
+                        showToast("密码不一致，请重新输入！");
+                        return;
+                    }
+                    showToast("注册成功！");
+                    setUser(username, psw);
+                    finish();
+                }else{
+                    showToast("请输入用户名或者密码！");
+                }
                 break;
         }
     }
